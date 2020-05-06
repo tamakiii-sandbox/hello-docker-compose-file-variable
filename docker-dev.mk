35d2b4437052c3c8e725769e98aae1ed8d33789e
tamakiii <tamakiii@users.noreply.github.com>
2020-05-06 12:23:02 +0900

    stop include

--- docker-dev.mk
+++ docker-dev.mk
@@ -1,10 +1,26 @@
-.PHONY: production-pseudo-base production-pseudo
+.PHONY: help install dependencies build clean
 
-include docker.mk
+help:
+	@cat $(firstword $(MAKEFILE_LIST))
+
+install: \
+	dependencies \
+	.env
+
+dependencies:
+	type docker > /dev/null
+	type docker-compose > /dev/null
 
 build: production-pseudo-base production-pseudo
 	bin/docker-compose-development build
 
+.env:
+	touch $@
+
+clean:
+	rm -rf clea
+
+.PHONY: production-pseudo-base production-pseudo
 production-pseudo-base:
 	TAG=$@ TARGET=$@ bin/docker-compose build
 
