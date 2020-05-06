a7fcac50ec45818d421bc3a6836218b6c8bf74cd
tamakiii <tamakiii@users.noreply.github.com>
2020-05-06 12:19:54 +0900

    fix docker.mk

--- docker.mk
+++ docker.mk
@@ -1,11 +1,5 @@
 .PHONY: help install dependencies build clean
 
-export DOCKER_BUILDKIT := 1
-export COMPOSE_DOCKER_CLI_BUILD := 1
-export COMPOSE_FILE := docker-compose.yml
-
-OPTIONS :=
-
 help:
 	@cat $(firstword $(MAKEFILE_LIST))
 
@@ -17,15 +11,11 @@ dependencies:
 	type docker > /dev/null
 	type docker-compose > /dev/null
 
-build up down:
-	$(call DOCKER_COMPOSE,$@,$(OPTIONS))
+build:
+	bin/docker-compose build
 
 .env:
 	touch $@
 
 clean:
 	rm -rf clean
-
-define DOCKER_COMPOSE
-		docker-compose $1 $2
-endef
